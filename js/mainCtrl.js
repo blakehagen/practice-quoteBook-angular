693357fd588be5e679b0bae3de9abad84fd5e43c
Blake Hagen <blakehagen@gmail.com>
2015-10-27 13:49:31 -0600

    step4 Done

--- js/mainCtrl.js
+++ js/mainCtrl.js
@@ -1,10 +1,28 @@
 angular.module('quoteBook')
 	.controller('mainCtrl', function ($scope, dataService) {
 
+
+
 		$scope.quotes = dataService.getData();
+
+		$scope.closeButtonsAdd = function () {
+			$scope.toggleRemove = false;
+			$scope.toggleFilter = false;
+		};
+
+		$scope.closeButtonsRemove = function () {
+			$scope.toggleAdd = false;
+			$scope.toggleFilter = false;
+		};
+
+		$scope.closeButtonsFilter = function () {
+			$scope.toggleAdd = false;
+			$scope.toggleRemove = false;
+		};
 
 
 		$scope.addQuote = function () {
+
 			var emptyObj = {};
 			emptyObj.text = $scope.quoteData;
 			emptyObj.author = $scope.authorData;
@@ -12,17 +30,17 @@ angular.module('quoteBook')
 			dataService.addData(emptyObj);
 			$scope.quoteData = '';
 			$scope.authorData = '';
+			$scope.toggleAdd = false;
 		};
 
 
 		$scope.removeQuote = function () {
 			dataService.removeData($scope.old)
 			$scope.old = '';
+			$scope.toggleRemove = false;
 		};
 
-		$scope.filter = function () {
 
-		}
 
 
 
